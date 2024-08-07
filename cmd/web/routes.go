bddbee100deb888e56981a5438c37d9d6cc7f9e3
Omar-Gebal <2001omaremad@gmail.com>
2024-08-07 17:48:39 +0300

    feat:db connection pooling and insert

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,10 +2,8 @@ package main
 
 import "net/http"
 
-// The routes() method returns a servemux containing our application routes.
 func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
-
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
